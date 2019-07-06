9db9f83b48b80befac8cc6d2f60aba6bb180e25e
Ross King <sleeping.stars@gmail.com>
2019-07-06 15:09:25 +0100

    Added template engine, views dir, index template, set static dir, added css

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 
+// Define dir for static files
+app.use(express.static('public'))
+
+// Set view engine to EJS
 app.set('view engine', 'ejs')
 
+// Handle get request to /
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // Send index.ejs
+  res.render('index')
 })
 
+// Start server listening port 3000
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!')
 })
